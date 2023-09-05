bb8072d363d025dd45cd9b8b423b1d6fb8529467
aarish21 <aarish.jerry@gmail.com>
2023-09-06 00:39:02 +0530

    Initial Commit

---
+++ college.io/college_ioApp.swift
@@ -0,0 +1,17 @@
+//
+//  college_ioApp.swift
+//  college.io
+//
+//  Created by Aarish on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct college_ioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
